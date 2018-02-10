2ec2c1e5094e15491106d60117c2bd79855f94ae
RafBro8 <rromn8@gmail.com>
2018-02-10 13:54:00 -0600

    created weather_list component + added sparkline charts(created component)

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,15 @@
+import { FETCH_WEATHER } from "../actions/index";
 
-
-export default function (state = null, action) {
+export default function (state = [], action) {
     //console.log('Action received', action);
+
+    switch (action.type) {
+        case FETCH_WEATHER:
+            return state.concat([action.payload.data]);
+            //concat returns new Array state
+            // ES6 Syntax  return [action.payload.data, ...state];
+            //we mainly care about 'payload.data' in return values
+    }
 
     return state;
 }
